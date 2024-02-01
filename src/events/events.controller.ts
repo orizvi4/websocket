315e5c82610a4be8d3ea3dc0b95b247a0204182e
orizvi4 <orizvi4@gmail.com>
2024-02-01 16:24:12 +0200

    added sign out

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -4,8 +4,14 @@ import { EventsGateway } from "./events.gateway";
 @Controller()
 export class eventsController {
     constructor(private eventsGateway: EventsGateway) {}
+
     @Post("channel/live")
-    async channelIsLive(@Body("_id") id: string, @Body("isLive") isLive: boolean) {//check with cameras
+    async channelIsLive(@Body("_id") id: string, @Body("isLive") isLive: boolean) {
         this.eventsGateway.emitter("channel_live",{_id: id, isLive: isLive});
+    }
+
+    @Post("user/signout")
+    async userSignout(@Body("username") username: string) {
+        this.eventsGateway.emitter("signout", username);
     }
 }
